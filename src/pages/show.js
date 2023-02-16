cafa144038b18eba5e44b5ad757e6ce78bb41747
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-15 20:15:47 -0500

    update form is on show page

--- src/pages/show.js
+++ src/pages/show.js
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Link, useLoaderData, Form } from "react-router-dom"
 
 const Show = (props) => {
     const post = useLoaderData()
@@ -13,6 +13,14 @@ const Show = (props) => {
     return <div style={div}>
         <h1>{post.subject}</h1>
         <h2>{post.details}</h2>
+        <div>
+            <h2>Update To-Do</h2>
+            <Form action={`/update/${post.id}`} method="post">
+            <input type="text" name="subject" placeholder="subject" defaultValue={post.subject}/>
+            <input type="text" name="details" placeholder="details" defaultValue={post.details}/>
+            <button>Update To-Do</button>
+        </Form>
+        </div>
         <Link to="/"><button>Go Back</button></Link>
     </div>
 }
