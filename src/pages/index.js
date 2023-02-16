667964350fb5c7c92806643f4904bc3d7c1b224b
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-15 20:06:08 -0500

    create functionality is done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Form } from "react-router-dom"
 import Post from "../components/post"
 
 const Index = (props) => {
     const todos = useLoaderData()
     console.log(todos)
-    return todos.map((post) => <Post key={post.id} post={post}/>)
+    return <> 
+    <div style={{textAlign: "center"}}>
+        <h2>Create a To-do</h2>
+        <Form action="/create" method="post">
+            <input type="text" name="subject" placeholder="subject"/>
+            <input type="text" name="details" placeholder="details"/>
+            <button>Create New To-do</button>
+        </Form>
+    </div>
+    {todos.map((post) => <Post key={post.id} post={post}/>)}
+    
+    </>
 }
 
 export default Index
